c5e44ef2a9c9a5d764fb96fc97dea43dea93d391
Wojciech Gawronski <afronski@gmail.com>
2015-05-30 18:46:07 +0200

    Adding users and removing insecure module.

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -23,29 +23,52 @@ if (Meteor.isClient) {
         "submit .new-task": function (event) {
             var text = event.target.text.value;
 
-            Tasks.insert({
-                text: text,
-                createdAt: new Date()
-            });
+            Meteor.call("addTask", text);
 
             event.target.text.value = "";
-
             return false;
         },
 
         "click .toggle-checked": function () {
-            Tasks.update(this._id, { $set: { checked: !this.checked } });
+            Meteor.call("setChecked", this._id, !this.checked);
         },
 
         "click .delete": function () {
-            Tasks.remove(this._id);
+            Meteor.call("deleteTask", this._id);
         },
 
         "change .hide-completed input": function (event) {
             Session.set("hideCompleted", event.target.checked);
         }
     });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
+    });
 }
+
+Meteor.methods({
+    addTask: function (text) {
+        if (!Meteor.userId()) {
+            throw new Meteor.Error("not-authorized");
+        }
+
+        Tasks.insert({
+            text: text,
+            createdAt: new Date(),
+            owner: Meteor.userId(),
+            username: Meteor.user().username
+        });
+    },
+
+    deleteTask: function (taskId) {
+        Tasks.remove(taskId);
+    },
+
+    setChecked: function (taskId, setChecked) {
+        Tasks.update(taskId, { $set: { checked: setChecked} });
+    }
+});
 
 if (Meteor.isServer) {
     Meteor.startup(function () {});
